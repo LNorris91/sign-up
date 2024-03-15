566eaf2b21fbb3fa21d486ab8c83de46d5f19e8c
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-03-15 17:50:43 -0400

    Create CSS
    this commit will create the CSS for the all the
    layout, colors, and fonts.

---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button")
+const link = document.querySelector("a")
+
+link.addEventListener('click', () => {
+    alert("This does nothing")
+})
+
+button.addEventListener('click', () => {
+    
+})
