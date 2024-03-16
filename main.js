2afa04a3ac1d421a5a250416f6aab01f9835bc34
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-03-16 16:03:08 -0400

    Create Javascript for Password Validation
    this commit adds all the javascript, which includes
    the password validation, and the log in link. This
    commit will also add placeholders for some of the
    inputs.

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 const button = document.querySelector("button")
 const link = document.querySelector("a")
+let pwd = document.querySelector("#pwd")
+let pwdConfirm = document.querySelector("#pwdConfirm")
+const errorMsg = document.querySelector(".errorMsg")
+const logInMsg = document.querySelector(".logInMsg")
 
-link.addEventListener('click', () => {
-    alert("This does nothing")
-})
+function validatePassword() {
+    if (pwd.value != pwdConfirm.value) {
+        pwdConfirm.style.cssText = "border-color: red;background-color: white;"
+        errorMsg.textContent = "Passwords don't match"
+        errorMsg.style.cssText = "color: red;"
+        button.disabled = true
+    } else if (pwd.value == pwdConfirm.value && pwd.value != ""){
+        pwdConfirm.style.cssText = "border-color: green;background-color: #e5fbe1"
+        errorMsg.style.cssText = "color: green;"
+        errorMsg.textContent = "They match!"
+        button.disabled = false
+    } else if (pwd.value == "" && pwdConfirm.value == "") {
+        errorMsg.textContent = ""
+        pwdConfirm.style.cssText = "border-color: rgb(210, 210, 210)"
+        button.disabled = false
+    }
+}
 
-button.addEventListener('click', () => {
-    
+link.addEventListener('click', (e) => {
+    alert("You can't log in because this isn't real and you can't create an account with something that doesn't exist.")
+    e.preventDefault()
 })
+button.addEventListener('mouseover', validatePassword)
+pwdConfirm.addEventListener('keyup', validatePassword)
